e24eaf29ebae45f3729d5fe426f39d39a19a03cb
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-11-06 16:58:08 +0100

    Set RabbitMQ version to 3.6.13 in Travis CI

--- bin/install_on_debian.sh
+++ bin/install_on_debian.sh
@@ -4,8 +4,8 @@ sudo apt-get update -y
 sudo apt-get install -y init-system-helpers socat adduser logrotate
 
 cd /tmp/
-wget https://dl.bintray.com/rabbitmq/rabbitmq-server-deb/rabbitmq-server_3.6.12-1_all.deb
+wget https://dl.bintray.com/rabbitmq/rabbitmq-server-deb/rabbitmq-server_3.6.13-1_all.deb
 sudo dpkg --install rabbitmq-server_3.6.12-1_all.deb
-sudo rm rabbitmq-server_3.6.12-1_all.deb
+sudo rm rabbitmq-server_3.6.13-1_all.deb
 
 sleep 3
