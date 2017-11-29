1967a73f9fc21d051c38345420206b8a7c2610d9
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-11-29 14:48:57 +0100

    Test against rabbitmq-server_3.7.0~alpha.600 on Travis CI

--- bin/install_on_debian.sh
+++ bin/install_on_debian.sh
@@ -4,8 +4,8 @@ sudo apt-get update -y
 sudo apt-get install -y init-system-helpers socat adduser logrotate
 
 cd /tmp/
-wget https://dl.bintray.com/rabbitmq/rabbitmq-server-deb/rabbitmq-server_3.6.14-1_all.deb
-sudo dpkg --install rabbitmq-server_3.6.14-1_all.deb
-sudo rm rabbitmq-server_3.6.14-1_all.deb
+wget https://dl.bintray.com/rabbitmq/debian-dev/pool/rabbitmq-server/rabbitmq-server_3.7.0~alpha.600-1_all.deb
+sudo dpkg --install rabbitmq-server_3.7.0~alpha.600-1_all.deb
+sudo rm rabbitmq-server_3.7.0~alpha.600-1_all.deb
 
 sleep 3
