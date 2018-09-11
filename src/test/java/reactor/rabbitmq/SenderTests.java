e48665b44b2978a3c35e90802a1f5cae1d07eb3e
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2018-09-11 09:46:21 +0200

    Add test case for #21
    References #21

---
+++ src/test/java/reactor/rabbitmq/SenderTests.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.rabbitmq;
+
+import com.rabbitmq.client.AlreadyClosedException;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ShutdownSignalException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static reactor.rabbitmq.ReactorRabbitMq.createSender;
+
+/**
+ *
+ */
+public class SenderTests {
+
+    Connection connection;
+    String queue;
+    Sender sender;
+
+    @BeforeEach
+    public void init() throws Exception {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.useNio();
+        connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+        String queueName = UUID.randomUUID().toString();
+        queue = channel.queueDeclare(queueName, false, false, false, null).getQueue();
+        channel.close();
+        sender = null;
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        if (connection != null) {
+            Channel channel = connection.createChannel();
+            channel.queueDelete(queue);
+            channel.close();
+            connection.close();
+        }
+        if (sender != null) {
+            sender.close();
+        }
+    }
+
+    @Test
+    void cannotReuseChannelOnError() {
+        sender = createSender();
+        try {
+            sender.declare(QueueSpecification.queue(queue).autoDelete(true)).block();
+        } catch (ShutdownSignalException e) {
+
+        }
+        try {
+            sender.declare(QueueSpecification.queue()).block();
+        } catch (AlreadyClosedException e) {
+
+        }
+    }
+}
