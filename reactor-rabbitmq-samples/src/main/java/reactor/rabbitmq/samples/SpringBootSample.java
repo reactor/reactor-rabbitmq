1fdb6010241d68c4599c887ab9f76f8df89a0f8a
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-02-04 11:25:34 +0100

    Add sample based on Spring Boot
    Fixes #52

---
+++ reactor-rabbitmq-samples/src/main/java/reactor/rabbitmq/samples/SpringBootSample.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2019 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.rabbitmq.samples;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Delivery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.rabbitmq.*;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@SpringBootApplication
+public class SpringBootSample {
+
+    static final String QUEUE = "reactor.rabbitmq.spring.boot";
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootSample.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSample.class, args).close();
+    }
+
+    // the queue will be created automatically by Spring AMQP
+    @Bean
+    Queue queue() {
+        return new Queue(QUEUE);
+    }
+
+    // the mono for connection, it is cached to re-use the connection across sender and receiver instances
+    // this should work properly in most cases
+    @Bean
+    Mono<Connection> connectionMono(ConnectionFactory connectionFactory) {
+        return Mono.fromCallable(() -> connectionFactory.createConnection().getDelegate()).cache();
+    }
+
+    @Bean
+    Sender sender(Mono<Connection> connectionMono) {
+        return RabbitFlux.createSender(new SenderOptions().connectionMono(connectionMono));
+    }
+
+    @Bean
+    Receiver receiver(Mono<Connection> connectionMono) {
+        return RabbitFlux.createReceiver(new ReceiverOptions().connectionMono(connectionMono));
+    }
+
+    @Bean
+    Flux<Delivery> deliveryFlux(Receiver receiver) {
+        return receiver.consumeNoAck(QUEUE);
+    }
+
+    // a runner that publishes messages with the sender bean and consumes them with the receiver bean
+    @Component
+    static class Runner implements CommandLineRunner {
+
+        final Sender sender;
+        final Flux<Delivery> deliveryFlux;
+        final AtomicBoolean latchCompleted = new AtomicBoolean(false);
+
+        Runner(Sender sender, Flux<Delivery> deliveryFlux) {
+            this.sender = sender;
+            this.deliveryFlux = deliveryFlux;
+        }
+
+        @Override
+        public void run(String... args) throws Exception {
+            int messageCount = 10;
+            CountDownLatch latch = new CountDownLatch(messageCount);
+            deliveryFlux.subscribe(m -> {
+                LOGGER.info("Received message {}", new String(m.getBody()));
+                latch.countDown();
+            });
+            LOGGER.info("Sending messages...");
+            sender.send(Flux.range(1, messageCount).map(i -> new OutboundMessage("", QUEUE, ("Message_" + i).getBytes())))
+                    .subscribe();
+            latchCompleted.set(latch.await(5, TimeUnit.SECONDS));
+        }
+
+    }
+
+}
