ec1369bda4e4a056cc00e9da0a834c6bbd7ffd79
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-06-18 10:09:39 +0200

    Create queue with AmqpAdmin in Spring AMQP sample
    Now the sample does not use a Spring AMQP connection, AmqpAdmin does not
    automatically create the resource beans.
    
    References #87

--- reactor-rabbitmq-samples/src/main/java/reactor/rabbitmq/samples/SpringBootSample.java
+++ reactor-rabbitmq-samples/src/main/java/reactor/rabbitmq/samples/SpringBootSample.java
@@ -21,6 +21,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Delivery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -33,6 +34,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.rabbitmq.*;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -45,15 +47,11 @@ public class SpringBootSample {
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootSample.class);
     @Autowired
     Mono<Connection> connectionMono;
+    @Autowired
+    AmqpAdmin amqpAdmin;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSample.class, args).close();
-    }
-
-    // the queue will be created automatically by Spring AMQP
-    @Bean
-    Queue queue() {
-        return new Queue(QUEUE);
     }
 
     // the mono for connection, it is cached to re-use the connection across sender and receiver instances
@@ -81,6 +79,11 @@ public class SpringBootSample {
     @Bean
     Flux<Delivery> deliveryFlux(Receiver receiver) {
         return receiver.consumeNoAck(QUEUE);
+    }
+
+    @PostConstruct
+    public void init() {
+        amqpAdmin.declareQueue(new Queue(QUEUE, false, false, true));
     }
 
     @PreDestroy
