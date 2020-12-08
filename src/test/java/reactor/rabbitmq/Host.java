f4ca842f4e6351f40114b7cd5a9502fa5e548937
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-12-08 17:17:42 +0100

    Add sudo_rabbitmqctl alias for Gradle
    (cherry picked from commit 461001164530b6dd239e905806dbcb4c1b48612a)

--- src/test/java/reactor/rabbitmq/Host.java
+++ src/test/java/reactor/rabbitmq/Host.java
@@ -95,8 +95,10 @@ public class Host {
         if (rabbitmqCtl.startsWith("DOCKER:")) {
             String containerId = rabbitmqCtl.split(":")[1];
             return "docker exec " + containerId + " rabbitmqctl";
+        } else if ("sudo_rabbitmqctl".equals(rabbitmqCtl)) {
+            return "sudo rabbitmqctl";
         } else {
-            return rabbitmqCtl;
+                return rabbitmqCtl;
         }
     }
 
